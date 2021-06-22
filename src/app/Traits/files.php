9fe21860cae3ffb3d6ee7373d0447fbd32fa4afc
Patryk Sawicki <patryk@patryksawicki.pl>
2021-06-22 17:33:17 +0200

    Files trait - Added option to get relation name.

--- src/app/Traits/files.php
+++ src/app/Traits/files.php
@@ -15,17 +15,18 @@ trait files
      * Add file.
      *
      * @param UploadedFile $file
-     * @param string $type e.g. order_files
+     * @param string $location e.g. order_files
+     * @param string $relationName e.g. files
      */
-    public function addFile(UploadedFile $file, string $type='files')
+    public function addFile(UploadedFile $file, string $location='files', string $relationName='files')
     {
         $fileName=$file->getClientOriginalName();
-        $filePath='/'.config('filesSettings.main_dir', 'hidden').'/'.$type.'/'.
+        $filePath='/'.config('filesSettings.main_dir', 'hidden').'/' . $location . '/' .
                   date('Y').'/'.date('m').'/'.date('d').'/';
         $extension=explode('.', $fileName);
         $extension=strtolower($extension[count($extension)-1]);
 
-        $fileModel=$this->files()->create([
+        $fileModel=$this->{$relationName}()->create([
             'name'=>$fileName,
             'type'=>$extension,
             'file'=>$filePath,
@@ -55,11 +56,12 @@ trait files
      * Add files.
      *
      * @param array $files
-     * @param string $type e.g. order_files
+     * @param string $location e.g. order_files
+     * @param string $relationName e.g. files
      */
-    public function addFiles(array $files, string $type='files')
+    public function addFiles(array $files, string $location='files', string $relationName='files')
     {
         foreach($files as $file)
-            $this->addFile($file, $type);
+            $this->addFile($file, $location, $relationName);
     }
 }
